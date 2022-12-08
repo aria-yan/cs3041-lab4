bbcd7ec9340a9211989225ef6abccc25ad797e68
aria-yan <93105892+aria-yan@users.noreply.github.com>
2022-12-08 16:48:45 -0500

    build

---
+++ src/screens/Home.jsx
@@ -0,0 +1,32 @@
+import {Button, TextField, Grid, Box} from '@mui/material';
+import logo from '../logo.svg';
+
+export function Home() {
+    return (
+		<Grid container spacing={2}>
+			<Grid item xs={12} justifyContent="center" alignItems="center" bgcolor="lightblue">
+                <h1 style={{color:'white'}}>Human Computer Interaction Newsletter</h1>
+			</Grid>
+			<Grid item xs={8} display="flex" justifyContent="center" alignItems="center">
+                <p>Signup for the human computer interaction newsletter!</p>
+                <TextField id="outlined-basic" type="email" label="email@wpi.edu" variant="outlined" style={{marginRight: '20px'}}/>
+                <Button variant='contained' onClick={Popup}>Sign Up</Button>
+			</Grid>
+			<Grid item xs={4} display="flex" justifyContent="center" alignItems="center">
+                <Box
+                    component="img"
+                    sx={{
+                        height: '100%',
+                        width: '100%',
+                    }}
+                    alt="A react logo"
+                    src={logo}
+                />
+            </Grid>
+		</Grid>
+	);
+}
+
+function Popup() {
+    alert('Signup successful!')
+}
